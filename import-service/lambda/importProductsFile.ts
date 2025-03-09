f57686774ca58baf79d9a2a3aebb59685668827e
Mariia <zubkovaa.mariya@yandex.ru>
2025-03-09 01:31:46 +0100

    task5 implement import product file

--- import-service/lambda/importProductsFile.ts
+++ import-service/lambda/importProductsFile.ts
@@ -1,8 +1,10 @@
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { APIGatewayEvent } from 'aws-lambda';
 
-export const handler = async (event: any) => {
+export const handler = async (event: APIGatewayEvent) => {
   try {
-    const filename = event.pathParameters?.filename;
+    const filename = event.queryStringParameters?.name;
 
     if (!filename) {
       const errorMessage = 'Missing file name';
@@ -20,13 +22,27 @@ export const handler = async (event: any) => {
       }
     }
 
-    // const client = new S3Client({ region });
-    // const command = new PutObjectCommand({
-    //   Bucket: process.env.BUCKET_NAME,
-    //   ContentType: "text/csv",
-    //   Key: `${filename}`
-    // });
-    //   return getSignedUrl(client, command, { expiresIn: 3600 });
+    const client = new S3Client();
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      ContentType: "text/csv",
+      Key: `uploaded/${filename}`
+    });
+    const signedUrl = await getSignedUrl(client, command, { 
+      expiresIn: 3600,
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ signedUrl }),
+    };
     
   } catch (error) {
     console.error(`Error processing event: ${error}`);
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -2,39 +2,64 @@ import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Runtime, Function, Code } from "aws-cdk-lib/aws-lambda";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-
+import { Bucket, EventType } from "aws-cdk-lib/aws-s3";
+import { LambdaDestination } from "aws-cdk-lib/aws-s3-notifications";
 
 export class ImportServiceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const bucketArn = "arn:aws:s3:::shop-app-uploaded";
-    const s3Bucket = Bucket.fromBucketArn(
-      this,
-      'ImportBucket',
-      bucketArn
-    )
+    const s3Bucket = Bucket.fromBucketArn(this, "ImportBucket", bucketArn);
 
-    const importProductsFile = new Function(this, "ImportProductsFileFunction", {
+    const importProductsFile = new Function(
+      this,
+      "ImportProductsFileFunction",
+      {
+        runtime: Runtime.NODEJS_18_X,
+        code: Code.fromAsset("lambda"),
+        handler: "importProductsFile.handler",
+        memorySize: 128,
+        timeout: Duration.seconds(5),
+        environment: {
+          BUCKET_NAME: s3Bucket.bucketName,
+        },
+      }
+    );
+
+    const importFileParser = new Function(this, "ImportFileParserFunction", {
       runtime: Runtime.NODEJS_18_X,
       code: Code.fromAsset("lambda"),
-      handler: "importProductsFile.handler",
+      handler: "importFileParser.handler",
       memorySize: 128,
-      timeout: Duration.seconds(5),
+      timeout: Duration.seconds(30),
       environment: {
         BUCKET_NAME: s3Bucket.bucketName,
-      }
+      },
     });
 
     s3Bucket.grantRead(importProductsFile);
+    s3Bucket.grantReadWrite(importFileParser);
+
+    s3Bucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new LambdaDestination(importFileParser),
+      { prefix: "uploaded/" }
+    );
 
     const api = new RestApi(this, "Import Service", {
       restApiName: "Shop Import Service",
       description: "This is the Import Service API",
+      defaultCorsPreflightOptions: {
+        allowOrigins: ["*"],
+        allowMethods: ["GET", "PUT", "POST"],
+        allowHeaders: ["*"],
+        exposeHeaders: [],
+        maxAge: Duration.days(1),
+      },
     });
 
     const importAPI = api.root.addResource("import");
-    importAPI.addResource("{filename}").addMethod("GET", new LambdaIntegration(importProductsFile));
+    importAPI.addMethod("GET", new LambdaIntegration(importProductsFile));
   }
 }
